193dda03975ad67e07c0f0edf6a53fafcd5d411d
Barath0011 <barathdilip63@gmail.com>
2023-10-04 10:56:08 +0000

    commit

--- reactapp/src/App.js
+++ reactapp/src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import{BrowserRouter,Routes,Route} from 'react-router-dom';
+import React from 'react';
+import Login from './Login';
+import Register from './Register';
+import NavBar from './layout/NavBar';
+import Footer from './layout/Footer';
+
+import Sidebar from './layout/Sidebar';
+import Home from './layout/Home';
+import Contact from './layout/Contact';
+import Services from './layout/Services';
+import Feedback from './layout/Feedback';
+import Privacy from './layout/Policy';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login/>}/>
+        <Route path="/register" element={<Register/>}/>
+        <Route path="/NavBar" element={<NavBar/>}/>
+        <Route path="/footer" element={<Footer/>}/>
+        <Route path="/que" element={<Sidebar/>}/>
+      <Route path="/product" element={<Home/>}/>
+      <Route path='/contact' element={<Contact/>}/>
+      <Route path='/service' element={<Services/>}/>
+      <Route path="/feed" element={<Feedback/>}/>
+      <Route path="/policy" element={<Privacy/>}/>
+       </Routes>
+     
+    
+    </BrowserRouter>
+  </div>
   );
 }
 
